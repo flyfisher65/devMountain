a98ecead88855e783928f0e1c9a2fc27a1e087e1
flyfisher65 <flyfisher65@gmail.com>
2015-09-26 16:53:54 -0600

    Saturday's first commit

---
+++ week2/day3/test.js
@@ -0,0 +1,13 @@
+var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
+
+
+ function looper(){
+ 	for(var i = 0; i < family.length; i++) {
+    	alert(family[i]);
+  }
+
+ }
+
+
+
+//Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
